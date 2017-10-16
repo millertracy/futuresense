4d92eb9c0dee31de5be3194a2b91e2114f9cb071
davidjayowens <david.jay.owens@gmail.com>
2017-10-16 11:41:31 -0700

    Testing OAuth2

--- futureflask.py
+++ futureflask.py
@@ -1,6 +1,49 @@
-from flask import Flask
+from flask import Flask, session, redirect
+from flask_oauthlib.client import OAuth
+import os
 
 app = Flask(__name__)
+oauth = OAuth()
+
+client_id = os.environ['DEX_CLIENT_ID']
+client_secret = os.environ['DEX_CLIENT_SECRET']
+redirect_uri = '34.215.61.65'
+
+dexcom = oauth.remote_app('dexcom',
+    base_url='https://sandbox-api.dexcom.com'
+    request_token_url=None
+    access_token_url='https://sandbox-api.dexcom.com/v1/oauth2/token'
+    authorize_url='https://sandbox-api.dexcom.com/v1/oauth2/login'
+    consumer_key=client_id
+    consumer_secret=client_secret
+)
+
+@dexcom.tokengetter
+def get_dexcom_token(token=None):
+    return session.get('dexcom_token')
+
+@app.route('/login')
+def login():
+    return dexcom.authorize(callback=url_for('oauth_authorized',
+                next=request.args.get('next') or request.referrer
+                or None))
+
+@app.route('/oauth-authorized')
+def oauth_authorized():
+    next_url = request.args.get('next') or url_for('index')
+    resp = dexcom.authorized_response()
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+
+    session['dexcom_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    )
+    session['dexcom_user'] = resp['user_name']
+
+    flash('You were signed in as %s' % resp['screen_name'])
+    return redirect(next_url)
 
 @app.route('/', methods=['GET'])
 def index():
